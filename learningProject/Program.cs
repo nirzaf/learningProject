03aa6e076211b377f0775821a879f8a463123b0a
Mohamed Farook Mohamed Fazrin <mohamed.farzin@idsnext.com>
2021-08-03 15:26:14 +0530

    Add project files.

---
+++ learningProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace learningProject
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var x = 25.32 / 12.33;
+            double y = 2;
+            var answer = x / y;
+            Console.WriteLine("Answer is {0}", x);
+        }
+    }
+}
