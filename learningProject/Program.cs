484ecf3c1f0d178c6470dceb0e19eb98284fffc4
Fazrin <nirzaf@gmail.com>
2021-08-06 23:29:30 +0530

    commit push

--- learningProject/Program.cs
+++ learningProject/Program.cs
@@ -6,10 +6,7 @@ namespace learningProject
     {
         private static void Main(string[] args)
         {
-            var x = 25.32 / 12.33;
-            double y = 2;
-            var answer = x / y;
-            Console.WriteLine("Answer is {0}", x);
+            Console.WriteLine("Learnig Loops Here");
         }
     }
 }
