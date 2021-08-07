bd8ff4f8c6dc8464e4a42d598c2546e083d71ea4
Fazrin <nirzaf@gmail.com>
2021-08-07 10:27:14 +0530

    commit

--- learningProject/Program.cs
+++ learningProject/Program.cs
@@ -6,7 +6,21 @@ namespace learningProject
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Learnig Loops Here");
+            Console.WriteLine("Enter your input here :");
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            for(int i=0; i<=input;i++)
+            {
+                if(i%3 ==0)
+                {
+                    Console.WriteLine('*');
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
         }
     }
 }
